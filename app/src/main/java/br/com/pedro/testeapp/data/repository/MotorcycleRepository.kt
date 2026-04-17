11368acf259804dad53b0333ad0f9ec2964c3baf
Pedro Henrique <pedroohenrique2009@hotmail.com>
2026-04-16 23:25:06 -0300

    adding comments.

--- app/src/main/java/br/com/pedro/testeapp/data/repository/MotorcycleRepository.kt
+++ app/src/main/java/br/com/pedro/testeapp/data/repository/MotorcycleRepository.kt
@@ -8,6 +8,7 @@ import br.com.pedro.testeapp.ui.Motorcycler
 class MotorcycleRepository {
 
     companion object {
+        //CHAVE API
         private const val API_KEY = "BbtSIv8ffe0TMSwzXG6N04EwwoU32NwiKjtrhqRE"
     }
 
